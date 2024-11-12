d13c4eaae4fa385610823db02e9ece68dedd0c44
matheusbitencourt <matheuslucas10@gmail.com>
2024-11-12 09:38:28 -0300

    chore: add prisma, database and tailwind prettier plugin.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">hello word</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,6 @@
+const Transactions = ()  => { 
+    return (<h1>Transactions page</h1>);
+  }
+
+export default Transactions;
+
