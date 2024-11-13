ab03ea3be261322dd1bc4cceef3828bd45a888d4
matheusbitencourt <matheuslucas10@gmail.com>
2024-11-12 23:40:45 -0300

    fix: update layout login.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,24 +6,25 @@ const LoginPage = () => {
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
-      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8"></div>
-      <Image
-        src="/logo.svg"
-        width={173}
-        height={39}
-        alt="Finance AI"
-        className="mb-8"
-      />
-      <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-      <p className="mb-8 text-muted-foreground">
-        A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-        monitorar suas movimentações, e oferecer insights personalizados,
-        facilitando o controle do seu orçamento.
-      </p>
-      <Button variant="outline">
-        <LogInIcon className="mr-2" />
-        fazer login ou criar conta
-      </Button>
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Finance AI"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
+          facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline">
+          <LogInIcon className="mr-2" />
+          fazer login ou criar conta
+        </Button>
+      </div>
       {/* DIREIRA */}
       <div className="relative h-full w-full">
         <Image
